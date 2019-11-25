0a55d70deed075860d3b1153509a4587d5fcd272
tejasVR <tejasshroff24@gmail.com>
2019-11-24 22:54:19 -0500

    Creating buttons to select project type

---
+++ Assets/Scripts/ProjectManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectManager : MonoBehaviour
+{
+
+    public enum ProjectType
+    {
+        Small,
+        Medium,
+        Large
+    }
+
+    public void SetProjectTimeline(ProjectType _projectType)
+    {
+
+    }
+
+    //private void 
+}
